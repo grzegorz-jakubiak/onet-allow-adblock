5533b0c9310b2c63708058a3b77fe04f115e2b14
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2022-03-16 21:42:55 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Onet Allow AdBlock
+
+Small user script to browse onet.pl without disabling AdBlock
---
+++ index.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name  Onet Allow AdBlock
+// @match https://*.onet.pl/*
+// @version 0.1
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  const highestTimeoutId = setTimeout(';')
+  for (let i = 0; i < highestTimeoutId; i++) {
+    clearTimeout(i)
+  }
+})()
